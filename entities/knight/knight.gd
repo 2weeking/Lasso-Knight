7fc7f8159ecc86d1f18d53c406de45fa3197857b
David <41875791+Astrol99@users.noreply.github.com>
2024-01-23 16:10:57 -0600

    Added curves to captured lasso ropes

--- entities/knight/knight.gd
+++ entities/knight/knight.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var lasso = $Lasso
 
+# key: value pair
+# {body: rope}
 var captured_enemies = {}
 
 func _physics_process(_delta):
@@ -23,7 +25,8 @@ func _physics_process(_delta):
 	# Captured enemies in dictionary {body: rope} pair
 	for body in captured_enemies:
 		var rope = captured_enemies[body]
-		rope.set_point_position(1, to_local(body.position))
+		#rope.set_point_position(1, to_local(body.position))
+		rope.points = lasso_to_curve(Vector2.ZERO, to_local(body.position))
 	
 	# Absorb Captured Enemies
 	var collision = get_last_slide_collision()
@@ -31,16 +34,27 @@ func _physics_process(_delta):
 		if collision.get_collider():
 			var body = collision.get_collider()
 			if body.is_in_group("enemies") and body.is_in_group("captured"):
-				# Delete rope
+				# Delete rope and enemy attached
 				captured_enemies[body].queue_free()
-				# Remove enemy and rope from captured enemy dictionary
-				captured_enemies.erase(body)
-				# Delete enemy attached
 				body.queue_free()
+				# Remove enemy and rope entries from captured_enemy dictionary
+				captured_enemies.erase(body)
 	
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+
+func lasso_to_curve(p0_vertex: Vector2, p1_vertex: Vector2):
+	var curve = Curve2D.new()
+	# Adjust curves to change sides
+	var p0_out = Vector2.ZERO
+	if p0_vertex.x > p1_vertex.x:
+		p0_out = Vector2(-50, 0)
+	else:
+		p0_out = Vector2(50, 0)
+	curve.add_point(p0_vertex, Vector2.ZERO, p0_out);
+	curve.add_point(p1_vertex, Vector2.ZERO, Vector2.ZERO);
+	return curve.get_baked_points()
 
 func reel_in(body):
 	# Instance new rope on knight
@@ -49,8 +63,9 @@ func reel_in(body):
 	rope.default_color = Color(0.588, 0.447, 0.349)
 	add_child(rope)
 	# Add rope point on knight and enemy and connect (relative position to knight)
-	rope.add_point(Vector2.ZERO)
-	rope.add_point(to_local(body.position))
+	rope.points = lasso_to_curve(Vector2.ZERO, to_local(body.position))
+	#rope.add_point(Vector2.ZERO)
+	#rope.add_point(to_local(body.position))
 	# Save captured enemies in array to continiously update in process function
 	captured_enemies[body] = rope
 
