b1a59ae6da031db786eb106ab5a82f742a7ee6c5
iqbatrg <109561812+iqbatrg@users.noreply.github.com>
2024-01-24 22:43:04 -0600

    Charger fix - dash
    -added dash
    
    to do:
    -make compatible with lasso
    -clean code if possible

--- entities/charger/Charger.gd
+++ entities/charger/Charger.gd
@@ -1,27 +1,36 @@
 extends CharacterBody2D
-@export var walkspeed = 50
-@export var runspeed = 70
+@export var walk_speed = 50
+@export var run_speed = 170
 @export var damage : int = 1
-var currentspeed = 50
 
 @onready var player = get_parent().get_node("Knight")
 @onready var timer = $Timer
 
+var current_speed = 50
+var make_charge = true
+var direction = Vector2(0, 0)
 func _ready():
 	timer.timeout.connect(_timeout)
 
 func _physics_process(delta):
-	if(!timer.is_stopped()):
+	if(timer.is_stopped()):
+		current_speed = walk_speed
 		timer.start(7)
-		currentspeed = walkspeed
+	
 	
 	if is_instance_valid(player):
 		#position = position.lerp(player.position, speed * delta)
-		var direction = (player.position - position).normalized()
-		velocity = direction * currentspeed
+		if(current_speed==walk_speed):
+			direction = (player.position - position).normalized()
+		velocity = direction * current_speed
 	
 	move_and_collide(velocity * delta)
 	
 func _timeout():
-	currentspeed = runspeed
-	timer.start(2)
+	if(make_charge):
+		current_speed = run_speed
+		make_charge = false
+		timer.start(2)	
+	else:
+		make_charge = true
+	
