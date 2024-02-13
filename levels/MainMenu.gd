2c15161d0c8ceccd3f250b7792d58b40975173f9
David <41875791+Astrol99@users.noreply.github.com>
2024-02-13 16:57:41 -0600

    Fixed change scene error on mainmenu

--- levels/MainMenu.gd
+++ levels/MainMenu.gd
@@ -4,7 +4,7 @@ func _ready():
 	randomize()
 
 func _on_Start_pressed():
-	if get_tree().change_scene_to_file("res://levels/Level1.tscn") != OK:
+	if get_tree().change_scene("res://levels/Level1.tscn") != OK:
 		print_debug("An unexpected error occured when trying to switch to the World scene")
 
 func _on_Quit_pressed():
