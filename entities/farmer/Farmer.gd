d00ee4dbe387376e04eca0eb7cb2eb7b7ad8b6f7
David <41875791+Astrol99@users.noreply.github.com>
2024-02-19 02:09:49 -0600

    Made farmer pissed animation

--- entities/farmer/Farmer.gd
+++ entities/farmer/Farmer.gd
@@ -1,9 +1,26 @@
 extends KinematicBody2D
 
+onready var current_scene = get_tree().get_current_scene()
 onready var sprite = $AnimatedSprite
 onready var cutscene = $Cutscene
 
+func areEnemiesRemaining() -> bool:    
+	# Iterate through all nodes in the scene
+	for node in current_scene.get_children():
+		# Check if the node belongs to the "enemy" group
+		if node.is_in_group("enemy"):
+			return true
+	
+	return false
+
+
 func _on_Hitbox_area_entered(area):
 	if area.name == "SenseRange":
-		cutscene.dialog = "Hey there young buck! You think you could help me out here in putting my cattle back in their pen? Thanks!"
-		cutscene.play_cutscene()
+		if areEnemiesRemaining():
+			cutscene.dialog = "Hey there young buck! I hate to be a bother, but I sure could use some help gettin' them cattle back in their pen. I appreciate it mister!"
+			cutscene.play_cutscene()
+		else:
+			sprite.flip_h = false
+			sprite.play("mad")
+			cutscene.dialog = "MY CATTLE! WHAT HAVE YOU DONE!!!!!!"
+			cutscene.play_cutscene()
